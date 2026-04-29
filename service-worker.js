1ce3d958036bfb0ed6d89978b4d3da83788dd02d
cryptosurfer75 <cryptosurfer75@gmail.com>
2026-04-29 20:06:15 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@
 //   SKIP_WAITING, et recharge automatiquement dès que le nouveau prend le
 //   contrôle — les utilisateurs ont ainsi la nouvelle version sans refresh manuel.
 
-const CACHE_VERSION = 'speedix-v187';
+const CACHE_VERSION = 'speedix-v188';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
